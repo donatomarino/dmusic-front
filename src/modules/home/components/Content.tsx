9385c341847a0c9c3e8509d9bb03376dac4a77af
donatomarino <donato_8@icloud.com>
2025-10-19 23:56:13 +0200

    fix

--- src/modules/home/components/Content.tsx
+++ src/modules/home/components/Content.tsx
@@ -1,17 +1,28 @@
 import { useContent } from '../hooks/useContent';
 
 const Content = () => {
-  const {artists} = useContent();
+  const { artists } = useContent();
 
   return (
-    <div className="bg-bg-cards h-full p-5">
-      <div className="text-center mb-8">
-        <h3 className="pt-5 text-2xl md:text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-blue-400 hover:animate-pulse">
-          ARTISTAS DEL MOMENTO
-        </h3>
-        <p className="text-sm text-gray-400 mt-1">Los más escuchados esta semana</p>
-        <div className="w-full h-1 bg-neutral-800 mx-auto mt-4 rounded-full"></div>
+    <div className="bg-bg-cards h-full p-4">
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-start gap-3 mb-4">
+        <div>
+          <img
+            src="https://i.pinimg.com/736x/8f/3b/1e/8f3b1eaa982f4f6d179e92c07cee99ea.jpg"
+            className="w-80 h-56 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl sm:h-32 md:h-40 lg:h-48 xl:h-48 object-cover rounded-xl"
+            alt="Banner"
+          />
+        </div>
+        <div>
+          <div className='flex flex-col'>
+            <h3 className="w-3xl text-center md:text-left sm:text-m md:text-l lg:text-xl xl:text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-blue-400 hover:animate-pulse">
+              ARTISTAS DEL MOMENTO
+            </h3>
+            <p className="text-m text-gray-400 mt-1">Los más escuchados esta semana</p>
+          </div>
+        </div>
       </div>
+        <div className="w-full h-1 bg-neutral-800 mx-auto mt-4 rounded-full"></div>
 
       <div className="mt-10 flex flex-wrap justify-center gap-6">
         {artists.slice(0, 4).map((e) => (
